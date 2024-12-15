db58bc14eb074aff209f0a494eae39891633d2b8
syedamaharukh <maharukhyousuf@gmail.com>
2024-12-15 17:22:49 +0500

    Add files via upload
    code push

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDom from "React-dom/client";
+
+const RootDiv = document.getElementById("root");
+
+ReactDom.createRoot(RootDiv).render(
+  <di>
+    <h1><strong><u>Hello React</u> </strong></h1>
+    <p>
+      React is a popular JavaScript library for building user interfaces, particularly single-page applications.
+      It was developed by Facebook and released as an open-source project in 2013.
+      React uses a component-based architecture, making code reusable and easier to manage.
+      It employs a virtual DOM for fast and efficient updates to the real DOM.
+      React supports declarative programming, allowing developers to describe what the UI should look like.
+      It integrates seamlessly with state management libraries like Redux or Context API.
+      React applications can be rendered on the client or server using ReactDOM or Next.js.
+      Its ecosystem includes tools like React Router for navigation and hooks for managing side effects and state.
+    </p>
+    <img width="400" src="https://kinsta.com/wp-content/uploads/2023/04/react-must-be-in-scope-when-using-jsx.jpg" alt="" />
+  </di>
+
+
+
+
+
+
+)
